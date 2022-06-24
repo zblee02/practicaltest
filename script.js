07e013179bc05c87b72ce0cc4ccc643f5e30117f
zblee02 <106216726+zblee02@users.noreply.github.com>
2022-06-24 16:59:40 +0800

    Lee Zhao Bang ptw practical test

---
+++ script.js
@@ -0,0 +1,10 @@
+function closeMe(){
+  x= document.getElementById("demo1");
+  //x.style.display="none";
+  x.className="closed";
+}
+function openMe(){
+  x=document.getElementById("demo1");
+  //x.style.display="block";
+  x.className="open";
+}
